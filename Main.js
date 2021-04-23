239b6e20cb8dcbcab9f6f4616f7e26af07606bc4
Tegsingh1 <78391323+Tegsingh1@users.noreply.github.com>
2021-04-23 09:34:07 -0400

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> Collage </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+
+<body>
+
+    <script src="Main.js"></script>
+    <nav class="navbar-inverse" style="position:sticky;top:0;z-index:99999">
+
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+        </div>
+
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="#Campus Life"> Campus Life </a></li>
+                <li><a href="#School Achievements">School Achievements</a></li>
+                <li><a href="#Environment">Environment</a></li>
+                <li><a href="#Books">Books</a></li>
+                <li><a href="#AboutME">About Me</a></li>
+            </ul>
+        </div>
+
+    </nav>
+
+    <h1> Our School </h1>
+
+
+    <div class="box">
+        <div class="imgbx">
+
+            <img src="https://webstockreview.net/images/conference-clipart-female-school-principal-1.png
+                                   " width="355px">
+
+        </div>
+        <div class="content">
+            <p class="P1"> There is currently 1 active confirmed case of Covid‑19 at Fairlawn Public School.
+                <br>
+                <br>
+                There is 1 classroom closed at this school due to a Covid‑19 exposure. If your child was in an affected class, you will be notified when they should return to school.
+            </p>
+        </div>
+
+    </div>
+
+
+    <div class="container">
+        <div class="page=header" id="Campus Life">
+            <h2 class="text-capitalize"> Campus Life </h2>
+            <div class="row">
+                <span class="travel_mainDiv">
+                    <img class="img-responsive" src="https://static.boredpanda.com/blog/wp-content/uploads/2017/05/590c8b688bbc2_OIqLjlb__605.jpg">
+                    <h4 class="text-capitalize"> <kbd> Slides In school </kbd> </h4>
+                    <hr>
+                    <h5 class="text-success text-left text capitalize"> Technical University Munich Put Slides In Their Building. </h5>
+
+                    <p class="text-muted text left"> Given that 65% of the population are visual learners, using slides or other visual support during a presentation falls somewhere between being pretty useful and critically important. For the 65%, visual reinforcement enhances their ability to digest, process, and ultimately retain material</p>
+
+   </span>
+                    <span class="travel_mainDiv">
+                        <img class="img-responsive" src="https://static.boredpanda.com/blog/wp-content/uploads/2017/05/59099ed49174d_q3zkbNm__605.jpg">
+                        <h4 class="text-capitalize"> <kbd> Math Doors </kbd> </h4>
+                        <hr>
+                        <h5 class="text-success text-left text capitalize"> Angles on door in math class </h5>
+
+                        <p class="text-muted text left"> An angle is a geometric shape formed by the intersection of two line segments, lines, or rays. Angles are a measure of rotational distance as contrasted with linear distance. ... Angles are important to defining and studying polygons such as triangles and quadrilaterals.</p>
+</span>
+
+                        <span class="travel_mainDiv">
+                            <img class="img-responsive" src="https://static.boredpanda.com/blog/wp-content/uploads/2017/05/59117dcf04d05_MrRXVK5__605.jpg">
+                            <h4 class="text-capitalize"> <kbd> Preschool </kbd> </h4>
+                            <hr>
+                            <h5 class="text-success text-left text capitalize">How My University Is Helping Us Get Over Exam Stress </h5>
+
+                            <p class="text-muted text left">
+                                Exams coming up? Sometimes the pressure of exams may cause you stress. Stress is your response to pressure and, while a small amount of pressure can be useful to keep you focused during exam time, if it becomes too much study can seem impossible.
+
+
+
+
+                        </span>
+                    </div>
+    
+            </div>
+        
+                <div class="page-header" id="School AchievementsSchool Achievements">
+            <h3> School Achievements </h3>
+        </div>
+        <ul class="list-group">
+            <li class="list-group-item list-group-item-success col-lg-12 col-md-12 col-sm-12 col-xs-12 Environment_li">
+                <h4> School Ranking </h4>
+               
+                <p class="list-group-item-text col-lg-7 col-md-7 col-sm-7 col-xs-12"> Canada is known for its high quality of life and world-renowned education. Thus, Williams Parkway Senior Public School is currently at 7/10 ranking and good ratings by parents and students.
+                </p>
+            </li>
+
+            <li class="list-group-item list-group-item-warning col-lg-12 col-md-12 col-sm-12 col-xs-12 Environment_li">
+                <h4> IBT info
+                </h4>
+                <p class="list-group-item-text col-lg-7 col-md-7 col-sm-7 col-xs-12"> “The International Business and Technology Program strives to integrate the objectives of 21st century skill development with project-based learning. Students will emerge from the IBT program as well-rounded citizens, equipped with the necessary skills, knowledge and attitudes to be productive participants in the global community.”
+                </p>
+            </li>
+
+        </ul>
+         <div class="page-header col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+            <h3> List of books</h3>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 book" data-toggle="modal" data-target="#first_space_Encyclopaedia
+"> <img src="s1.jpg" class="img-responisve">
+            <hr>
+            <p class="text-primary"> First Space Encyclopaedia </p>
+        </div>
+
+        <div class="modal_fade" id="first_space_Encyclopaedia">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header"> <button class="close" type="button" data-dismiss="modal"> </button>
+                        <h4 class="modal-title"> First Space Encyclopaedia </h4>
+                    </div>
+                    <div class="modal-body"> <img src="s1.jpg" class="img-responsive img_modal">
+                        <p class="p_modal">Zoom off into the galaxy and investigate all the planets, from Earth to Mars with First Space Encyclopaedia. Packed full of stunning photography, quizzes and amazing facts, children will become space experts in no time. Covering everything from observatories and rockets, to the solar system and the death of stars, children will discover all about space and space travel.<br> <a href="https://www.amazon.in/First-Space-Encyclopaedia-DK/dp/0241293421/ref=sr_1_6?crid=176WU3JFDK2JP&keywords=books+on+space+for+kids&qid=1575444723&sprefix=books+on%2Caps%2C285&sr=8-6"><b>BEST BUY LINK</b></a> </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+
+
